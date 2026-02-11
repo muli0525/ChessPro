b03031d7540707a79292bbca963d1c93a8280780
ChessPro <chesspro@example.com>
2026-02-11 19:23:14 +0800

    Fix GitHub Actions - add chmod +x gradlew

---
+++ add_gradlew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /f/test/ChineseChessPro
+chmod +x gradlew
+git add gradlew
+git commit -m "Add gradlew for Linux/Mac (GitHub Actions)"
+git push
---
+++ fix_chmod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /f/test/ChineseChessPro
+git add .
+git commit -m "Fix GitHub Actions - add chmod +x gradlew"
+git push
